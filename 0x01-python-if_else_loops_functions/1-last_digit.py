e79728d9dec02ad2da950189c80034868e828238
Anthon002 <chineduanulugwo@gmail.com>
2023-06-06 05:59:02 -0700

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if (number < 0):
     bakToNum = bakToNum * (-1)
 if (bakToNum > 5):
     print(f"Last digit of {number} is {bakToNum} and is greater than 5")
-elif(bakToNum == 0 ):
+elif(bakToNum == 0):
     print(f"Last digit of {number} is {bakToNum} and is 0")
 else:
     print(f"Last digit of {number} is {bakToNum} and is less than 6 and not 0")
