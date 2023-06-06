2f3a8756972d9308feca2e0b7bd65a86f8a62f60
Anthon002 <chineduanulugwo@gmail.com>
2023-06-06 05:55:14 -0700

    Last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 numToStr = str(number)
 bakToNum = int(numToStr[-1])
+if (number < 0):
+    bakToNum = bakToNum * (-1)
 if (bakToNum > 5):
     print(f"Last digit of {number} is {bakToNum} and is greater than 5")
 elif(bakToNum == 0 ):
