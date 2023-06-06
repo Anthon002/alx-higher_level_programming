e0047031c366d056d49eec460a86f8b16bb12417
Anthon002 <chineduanulugwo@gmail.com>
2023-06-06 12:38:21 -0700

    Remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return string
+
+    new_string = ""
+    for i, char in enumerate(str):
+        if i != n:
+            new_string += char
+
+    return new_string
