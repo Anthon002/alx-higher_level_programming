1f69f1ab6b1e2d9f53c23b0e36c6ec6195493e08
Anthon002 <chineduanulugwo@gmail.com>
2023-06-08 05:42:44 -0700

    variable no. of args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+import sys
+if __name__ == "__main__":
+    i = len(sys.argv) - 1
+    k = 0
+    if (i == 0):
+        print("{} arguments.".format(i))
+    elif (i > 1):
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+    for j in range(i):
+        k += 1
+        print("{}: {}".format(k, sys.argv[k]))
