93383d3d7d28268557d4a5d7e7e6f8fe79f0b0b1
Anthon002 <chineduanulugwo@gmail.com>
2023-06-08 08:38:34 -0700

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+from calculator_1 import *
 import sys
-def hand():
+if __name__ == "__main__":
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        operas = {
-        "+": (add, "+"),
-        "-": (sub, "-"),
-        "*": (mul, "*"),
-        "/": (div, "/")
-        }
-        opera = sys.argv[2]
-        if opera not in operas:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    operators = {"/": div, "-": sub, "+": add, "*": mul}
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            result = operas[opera][0](a, b)
-            opera_symbol = operas[opera][1]
-            print("{} {} {} = {}".format(a, opera_symbol, b, result))
-
-
-if __name__ == "__main__":
-    hand()
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
