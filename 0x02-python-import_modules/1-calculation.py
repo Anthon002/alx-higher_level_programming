03e250e91c3fa1854b1242be6eb772b51d754453
Anthon002 <chineduanulugwo@gmail.com>
2023-06-08 05:12:30 -0700

    calc all

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import *
 if __name__ == "__main__":
-    def calculate_all():
-        a = 10
-        b = 5
-        c =[add(a, b), sub(a, b), mul(a, b), div(a, b)]
-        for i in range(len(c)):
-            print("{} + {} = {}".format(a, b, c[i]))
-    calculate_all()
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
