1c419d95e08025cbb66bcf132da602742c1fd820
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-06-14 22:58:42 +0100

    Update 101-square_matrix_map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_comprehension(matrix=[]):
-    sqrd_mtrx = [[elem**2 for elem in i] for i in matrix]
-    return (sqrd_mtrx)
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda i: i**2, x)), matrix)))
