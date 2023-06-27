a1cdb643ea3432fa08ad3c75ce4496ba014c740d
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-06-27 20:32:14 +0100

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-''' an empty class Square that defines a square'''
+"""an empty class Square that defines a square"""
 
 
 class Square:
-  ''' The empty class'''
+  """The empty class"""
   pass
