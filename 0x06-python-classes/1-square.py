fc1bedf78b61e169849bec083e3395c973184442
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-06-27 20:35:22 +0100

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python3
+
+"""Implementation of a Square class."""
+
+
 class Square:
+    """Represents a square shape.
+
+    Attributes:
+        __size (int): The side length of the square.
+    """
+
     def __init__(self, size):
-        self._Square__size = size
+        """Initialize a new square.
+
+        Args:
+            size (int): The side length of the square.
+        """
+        self.__size = size
