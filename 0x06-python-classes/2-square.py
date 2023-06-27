da2a239c3aec8128c2a029083580af4cda45fa36
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-06-27 20:37:59 +0100

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,51 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class representing a square shape.
+"""
+
+
 class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The side length of the square.
+    """
+
     def __init__(self, size=0):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The side length of the square.
+        """
         self.__size = 0
         self.size = size
 
     @property
     def size(self):
-        return (self.__size)
+        """
+        Get the size of the square.
+
+        Returns:
+            int: The side length of the square.
+        """
+        return self.__size
 
     @size.setter
-    def size(self, val):
-        if not isinstance(val, int):
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The side length of the square.
+
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif val < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = val
+        self.__size = value
