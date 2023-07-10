00aec04027f7866ccba068480dea8b58b5b76435
Anthon002 <chineduanulugwo@gmail.com>
2023-07-10 07:49:21 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module has lookup function to print all attributes and methods of obj
+"""
+
+def lookup(obj):
+    """
+    This funciton returns the list of attributes and methods of an obj
+
+    Arg:
+        obj:object
+
+    Return:
+        obj
+    """
+    return dir(obj)
+
+    
