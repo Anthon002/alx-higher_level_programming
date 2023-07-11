0a49b811ea0f1ce65278b1b133049b937a27b2f0
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-07-11 20:24:41 +0100

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -15,9 +15,11 @@ class Student:
     def to_json(self, attrs=None):
         """this function reps the students class in a dictionary format.
         """
-        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return json.loads(json.dumps(self.__dict__))
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, i) for i in attrs if hasattr(self, i)}
+        self_dict = self.__dict__
+        return (self_dict)
         
     def reload_from_json(self, json):
         """This function will  replace all attributes of the Student instance.
