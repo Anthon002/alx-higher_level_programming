249c797d671ad0a5f0d8e543bf223c6742da3efe
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-07-11 20:09:37 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a class student."""
+
+
+class Student:
+    """this class defines a new student."""
+
+    def __init__(self, first_name, last_name, age):
+        """this is the initializer for the student class.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """represents the students in a dict format."""
+                if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
