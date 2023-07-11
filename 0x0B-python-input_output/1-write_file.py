f9d8904b2d293e3d0e43af27cb7604dc0a8c7f94
Anthon002 <chineduanulugwo@gmail.com>
2023-07-11 07:13:11 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,15 @@
 """
 
 def write_file(filename="", text=""):
+    """
+    This function writes to the filename file
+    
+    Args: 
+        filename(str): name of the file to be read from
+        text(str): what to write to the file
+
+    Return: length of text
+    """
     with open(filename, 'w', encoding = "utf-8") as the_file:
         the_file.write(text)
     return (len(text))
